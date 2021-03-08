9ffff42ddf35839db840e02f17a36ec804733d18
gopalkgm <gopal_gkm@yahoo.com>
2021-03-08 14:29:12 -0800

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail>  PaymentDetails { get; set; }
+
+    }
+}
